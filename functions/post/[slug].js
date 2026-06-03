aee8c8e7b359e97f5a992135f05648c25095c841
Your <scarlet3628@likelion.org>
2026-06-03 12:12:53 +0900

    css

--- functions/post/[slug].js
+++ functions/post/[slug].js
@@ -308,7 +308,7 @@ export async function onRequestGet({ params, env, request }) {
   <meta name="twitter:description" content="${escapeHtml(descriptionText)}" />
   <meta name="twitter:image" content="${escapeHtml(ogImage)}" />
 
-  <link rel="stylesheet" href="/assets/css/app.css?v=20260523v31" />
+  <link rel="stylesheet" href="/assets/css/app.css?v=20260603v14" />
   <link rel="stylesheet" href="/assets/css/components.css?v=20260429v1" />
 
   ${jsonld(blogPostingJsonLd)}
@@ -1054,7 +1054,7 @@ function renderNotFound(slug) {
   <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/favicon-192x192.png" />
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png" />
   <meta name="theme-color" content="#B8875B" />
-  <link rel="stylesheet" href="/assets/css/app.css?v=20260523v31" />
+  <link rel="stylesheet" href="/assets/css/app.css?v=20260603v14" />
   <link rel="stylesheet" href="/assets/css/components.css?v=20260429v1" />
 </head>
 <body>
