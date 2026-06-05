de819d618c679f1a828a5f85467c5200c2cc98a7
Your <scarlet3628@likelion.org>
2026-06-05 14:22:32 +0900

    modified post style

--- functions/post/[slug].js
+++ functions/post/[slug].js
@@ -3,7 +3,7 @@ import { renderMarkdown, renderMarkdownBlocks, buildTocItemsFromBlocks, renderTo
 import { buildImageAttrs } from "../../lib/image-utils.js";
 
 const SITE_ORIGIN = "https://wacky-travel.pages.dev";
-const POST_RENDER_VERSION = "20260605-post-guide-style-v1";
+const POST_RENDER_VERSION = "20260605-post-guide-style-v2";
 
 
 export async function onRequestGet({ params, env, request }) {
@@ -306,7 +306,7 @@ export async function onRequestGet({ params, env, request }) {
   <meta name="twitter:description" content="${escapeHtml(descriptionText)}" />
   <meta name="twitter:image" content="${escapeHtml(ogImage)}" />
 
-  <link rel="stylesheet" href="/assets/css/app.css?v=20260605postguide1" />
+  <link rel="stylesheet" href="/assets/css/app.css?v=20260605postguide2" />
   <link rel="stylesheet" href="/assets/css/components.css?v=20260429v1" />
   <style>
     .post-body,
@@ -1071,7 +1071,7 @@ function renderNotFound(slug) {
   <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/favicon-192x192.png" />
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png" />
   <meta name="theme-color" content="#B8875B" />
-  <link rel="stylesheet" href="/assets/css/app.css?v=20260605postguide1" />
+  <link rel="stylesheet" href="/assets/css/app.css?v=20260605postguide2" />
   <link rel="stylesheet" href="/assets/css/components.css?v=20260429v1" />
 </head>
 <body>
