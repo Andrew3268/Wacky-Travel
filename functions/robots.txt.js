633b37357800c75a3cc543bd5b89a2ee5adf1777
Steve Lee <scarlet3628@gmail.com>
2026-09-14 15:14:50 +0900

    daum

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -7,6 +7,8 @@ Allow: /
 Disallow: /api/
 
 Sitemap: ${origin}/sitemap.xml
+
+#DaumWebMasterTool:6886e2d0875cfb383dee7223024f130e4f47a83bb9ff86e649f7502064e23bf5:pa0rUlP4x9Rpwmt9hkdpVA==
 `;
   return new Response(body, {
     headers: {
